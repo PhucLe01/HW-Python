5fba8949bd85a6e2573da14a80b4ba0ccb8849e5
Phuc <phuc.le01@sjsu.edu>
2021-10-10 19:20:52 -0700

    change location of validating input operator from parse_input() to calculator() method

--- calculator.py
+++ calculator.py
@@ -1,18 +1,22 @@
 def calculator(number1, number2, operator):
     number1 = float(number1)
     number2 = float(number2)
-    if(operator == "+"):
-        return number1 + number2
-    elif(operator == "-"):
-        return number1 - number2
-    elif(operator == "*"):
-        return number1 * number2
-    elif(operator == "/"):
-        return number1 / number2
-    elif(operator == "//"):
-        return number1 // number2
-    elif(operator == "**"):
-        return number1 ** number2
+    validOperator = ["+", "-", "*", "/", "//", "**"]
+    if(operator in validOperator):
+        if(operator == "+"):
+            return number1 + number2
+        elif(operator == "-"):
+            return number1 - number2
+        elif(operator == "*"):
+            return number1 * number2
+        elif(operator == "/"):
+            return number1 / number2
+        elif(operator == "//"):
+            return number1 // number2
+        elif(operator == "**"):
+            return number1 ** number2
+    else:
+        return False
 
 def parse_input():
     eq = input("Enter equation: ")
@@ -20,8 +24,4 @@ def parse_input():
     in1 = eqList[0]
     op = eqList[1]
     in2 = eqList[2]
-    validOperator = ["+", "-", "*", "/", "//", "**"]
-    if(op in validOperator):
-        return calculator(in1, in2, op)
-    else:
-        return False
+    calculator(in1, in2, op)
